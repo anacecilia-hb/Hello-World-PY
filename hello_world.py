d9188e5432d0ec024dfa5fecddd1cb92146224c2
AnaCecília <ana.batista83@aluno.ce.gov.br>
2023-08-11 20:12:34 -0300

    Primeiro-Projeto-Py

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#exibe a informação do parâmetro
+print("Hello World")
+
+#abaixo junta os números
+first_number = input("O primeiro número: ")
+second_number = input("O segundo número: ")
+
+result = first_number + second_number
+
+print(result)
+
+#abaixo efetua a conta
+first_number = input("O primeiro valor: ")
+second_number = input("O segundo valor: ")
+
+result = (int(first_number) + int(second_number))
+
+print(result)
